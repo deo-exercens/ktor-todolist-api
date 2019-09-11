f5269baaafb5d28bc9657af944a4c2f34130b359
Mincheol Kim <mcauto@naver.com>
2019-09-11 14:09:35 +0900

    init project

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package com.exercens.deo
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("hello world!")
+        }
+    }
+}
+
